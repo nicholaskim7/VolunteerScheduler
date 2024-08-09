8baeb2b7c8f8b45e6b2394aa1ef22787abc44f25
nicholaskim7 <154484441+nicholaskim7@users.noreply.github.com>
2024-08-09 10:15:20 -0500

    Update UserProfile.jsx

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -44,27 +44,27 @@ function Profile() {
     };
 
     return (
-        <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
-            <div className='w-50 bg-white rounded p-3'>
+        <div className='d-flex vh-100 bg-green justify-content-center align-items-center'>
+            <div className='w-50 custom-box rounded p-3'>
                 <h2>Update Profile</h2>
                 <form onSubmit={handleProfileUpdate}>
                     <div className='mb-2'>
                         <label>Name</label>
-                        <input type="text" name="full_name" className='form-control'
+                        <input type="text" name="full_name" className='form-control custom-box'
                             value={user.full_name || ''}
                             onChange={handleChange}
                         />
                     </div>
                     <div className='mb-2'>
                         <label>Username</label>
-                        <input type="text" name="username" className='form-control'
+                        <input type="text" name="username" className='form-control custom-box'
                             value={user.username || ''}
                             onChange={handleChange}
                         />
                     </div>
                     <div className='mb-2'>
                         <label>Update Profile Picture</label>
-                        <input type='file' className='form-control' onChange={handleFileChange} />
+                        <input type='file' className='form-control custom-box' onChange={handleFileChange} />
                         {user.profile_picture && <img src={`http://localhost:8081${user.profile_picture}`} alt="Profile" width="100" />}
                     </div>
                     <button className='btn btn-success'>Update Profile</button>
